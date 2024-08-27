a79ec86be5fbacf31d85b9bdad0fefac8a6769a8
Robert Jones II <rjones@corporatewebimage.com>
2024-08-26 17:27:05 -0700

    Removed parent::load() call

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,10 +7,8 @@ use WebImage\Application\ApplicationInterface;
 
 class Plugin extends AbstractPlugin
 {
-	public function load(ApplicationInterface $app): void
+	protected function load(ApplicationInterface $app): void
 	{
-		parent::load($app);
-
 		$app->getServiceManager()->addServiceProvider(new DatabaseServiceProvider);
 	}
 }
